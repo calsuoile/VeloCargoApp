5f238b41944ee60a172032db0f8ee6c882068603
Charles-Hugo Vialle <charly.vialle@gmail.com>
2021-06-08 14:07:20 +0200

    architecture des composants

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import ContactForm from '../sources/view/contactForm/ContactForm'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Component {...pageProps} />
+  <ContactForm/>
+  </>
+  )
 }
 
 export default MyApp
