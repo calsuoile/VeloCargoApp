054c73a7d03490d330f1e87a8ada649d4af03695
arne0512 <arn.clerc@gmail.com>
2021-06-11 09:31:02 +0200

    maj artciles page

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,23 @@
 import React from "react";
 import ListArticles from "../sources/view/articles/view/ListArticles";
-import ListArticlesHomePage from "../sources/view/articles/view/ListArticlesHomePage";
 import ViewArticle from "../sources/view/articles/view/ViewArticle";
 import "../styles/globals.css";
 import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/styles";
-import ContactForm from "../sources/view/contactForm/ContactForm";
 import "../styles/globals.css";
 import theme from "../sources/theme";
 import Home from "../sources/view/home/Home";
+import ListArticlesHomePage from "../sources/view/articles/view/ListArticlesHomePage";
 
 function MyApp() {
   return (
     <>
-      <ListArticlesHomePage />
-      {/* <ViewArticle /> */}
-      {/* <ListArticles /> */}
       <ThemeProvider theme={theme}>
         <CssBaseline />
-     <Home />
-
+        <Home />
+        {/* <ViewArticle /> */}
+        <ListArticles />
+        {/* <ListArticlesHomePage /> */}
         {/* <ContactForm/> */}
       </ThemeProvider>
     </>
--- sources/view/home/Home.jsx
+++ sources/view/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ListArticlesHomePage from '../articles/view/ListArticlesHomePage';
 import CardAdsList from './CardAdsList';
 
 function Home(props) {
     return (
         <div>
             <CardAdsList />
+            <ListArticlesHomePage />
         </div>
     );
 }
