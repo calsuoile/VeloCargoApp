7b7853d3dae5815861dc566b9418f359f908d2ac
Anaïs Jarnac <ajarnac@gmail.com>
2021-07-12 17:06:48 +0200

    maj

--- sources/layout/Header/components/AboutButton.jsx
+++ sources/layout/Header/components/AboutButton.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Link from "next/link";
-<<<<<<< HEAD
 import { Typography } from "@material-ui/core";
-=======
-import Typography from "@material-ui/core/Typography";
->>>>>>> origin/dev
 
 const useStyles = makeStyles((theme) => ({
   root: {
